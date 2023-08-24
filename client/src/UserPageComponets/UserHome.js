fa686b8c0adb8bf5dfbaab7f602d1ad2c34b558c
Sam Peck <fullstacksam@protonmail.com>
2023-08-24 17:59:10 -0400

    started user page

--- client/src/UserPageComponets/UserHome.js
+++ client/src/UserPageComponets/UserHome.js
@@ -1,8 +1,33 @@
-function UserHome(){
+function UserHome({user}){
+console.log('user in userHOme is', user)
+
+const userWins = user.totalGuessesCorrect;
+const userLoses = user.totalGuessesIncorrect;
+
+function mapPredictions(){
+ return user.User_Predictions.map((prediction) =>{
+      return(
+        <div className="WrapperSinglePrediction">
+            <span className="predictionSpan">{`You Guessed ${prediction.predictedWinnerId}
+             would win ${prediction.game_Id}
+             and ${prediction.actualWinnerId} Won
+             `}
+            
+            </span>
+
+        </div>
+      )
+  })
+}
 
   return (
     <div>
-      <h1>Welcome home</h1>
+      <h1>Welcome home {user.username}</h1>
+      <h4>Your  {user.User_Predictions.length} predictions</h4>
+      <h4>You guess correctly "cant put anything here untill resolutions are fixed" % of the time</h4>
+      <h4>You have made {user.totalGuessesCorrect} Correct predictions</h4>
+      <h4>You have made {user.totalGuessesIncorrect} Incorrect predictions</h4>
+      {mapPredictions()}
     </div>
   )
 }
