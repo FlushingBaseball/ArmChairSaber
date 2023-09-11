3b8b0a5b2f76dfed3e871a454e0634a3774a179e
Sam Peck <fullstacksam@protonmail.com>
2023-09-10 23:57:27 -0400

    ok

--- client/src/PlayerComponets/PlayerPage.js
+++ client/src/PlayerComponets/PlayerPage.js
@@ -6,7 +6,12 @@ import Search from "../UtilityComponets/Search"
 export default function PlayerPage(){
 
 const  [searchPlayer, setSearchPlayer] = useState('');
-const [fetchedPlayers, setFetchedPlayers] = useState('')
+const [fetchedPlayers, setFetchedPlayers] = useState('');
+const [playerData, setPlayerData] = useState('');
+
+const fielderImage =searchPlayer ? `https://img.mlbstatic.com/mlb-photos/image/upload/v1/people/${searchPlayer}/headshot/silo/current` : '/Images/default-batter.svg'
+
+
 
 useEffect(()=>{
   fetch('/players')
@@ -15,6 +20,24 @@ useEffect(()=>{
       setFetchedPlayers(data)
   })
 },[])
+
+
+useEffect(()=>{
+  fetch(`https://baseballsavant.mlb.com/players-services/range?playerId=${searchPlayer}&season=2023`) // not the right endpoint
+      .then((resp) => resp.json())
+      .then(data => {setPlayerData(data)
+      })
+},[searchPlayer])
+
+
+
+useEffect(()=>{
+  console.log(searchPlayer)
+  console.log(playerData)
+},[searchPlayer,playerData])
+
+
+  console.log(searchPlayer)
 
   return (
     <div className="PlayerWrapper">
