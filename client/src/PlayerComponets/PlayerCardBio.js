20bf6c57cf23ed75c8ea9cd8f244e7f367aba7d9
Sam Peck <fullstacksam@protonmail.com>
2023-09-14 23:47:43 -0400

    working on card still

--- client/src/PlayerComponets/PlayerCardBio.js
+++ client/src/PlayerComponets/PlayerCardBio.js
@@ -6,8 +6,18 @@ export default function PlayerCardBio({searchPlayer, playerData}){
   return (
     <div className="WrapperPlayerCardBio">
         <img id="PlayerCardImage" src={fielderImage}></img>
-        {console.log('This is player data',playerData)}
-        <span>{playerData.people[0].active}</span>
+        {console.log('This is player data people[0]',playerData.people[0])}
+        {/* <span>{playerData.people[0].active}</span> */}
+        <h3>{playerData.people[0].firstLastName}</h3>
+        <span className="bioSpan">{`Age: ${playerData.people[0].currentAge}`}</span>
+        <span className="bioSpan">{`Drafted: ${playerData.people[0].draftYear}`}</span>
+        <span className="bioSpan">{`Birth Date: ${playerData.people[0].birthDate}`}</span>
+        <span className="bioSpan">{`Birth Place: ${playerData.people[0].birthCity}`}</span>
+        <span className="bioSpan">{`${playerData.people[0].birthStateProvince}`}</span>
+        <span className="bioSpan">{`Bats: ${playerData.people[0].batSide.description}`}</span>
+        <span className="bioSpan">{`Bats: ${playerData.people[0].batSide.description}`}</span>
+        <span className="bioSpan">{`Bats: ${playerData.people[0].batSide.description}`}</span>
+        {}
     </div>
   )
   }
