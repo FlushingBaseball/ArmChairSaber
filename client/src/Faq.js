a81045aa73ead6ca8805dbd0dacc6fb006f61ce9
Sam Peck <fullstacksam@protonmail.com>
2023-10-03 23:23:45 -0400

    faq

---
+++ client/src/Faq.js
@@ -0,0 +1,45 @@
+export default function Faq(){
+
+
+  return (
+    <div id="WrapperFaq">
+      <h1>Frequently asked Questions</h1>
+        <div className="WrapperQuestion">
+          <h3></h3>
+          <p></p>
+        </div>
+        <div className="WrapperQuestion">
+          <h3></h3>
+          <p></p>
+        </div>
+        <div className="WrapperQuestion">
+          <h3></h3>
+          <p></p>
+        </div>
+        <div className="WrapperQuestion">
+          <h3></h3>
+          <p></p>
+        </div>
+        <div className="WrapperQuestion">
+          <h3></h3>
+          <p></p>
+        </div>
+        <div className="WrapperQuestion">
+          <h3></h3>
+          <p></p>
+        </div>
+        <div className="WrapperQuestion">
+          <h3></h3>
+          <p></p>
+        </div>
+        <div className="WrapperQuestion">
+          <h3></h3>
+          <p></p>
+        </div>
+        <div className="WrapperQuestion">
+          <h3></h3>
+          <p></p>
+        </div>
+    </div>
+  )
+}
