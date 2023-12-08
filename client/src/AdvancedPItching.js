4a0748b5b19e0c613257374b87b5c60bb94d66c1
Sam Peck <fullstacksam@protonmail.com>
2023-12-08 03:26:15 -0500

    Temporary AB test finished, place holder untill new

--- client/src/AdvancedPItching.js
+++ client/src/AdvancedPItching.js
@@ -14,18 +14,19 @@ import { useEffect, useState } from "react";
 function AdvancedPitching() {
   const [fetchedGameData, setFetchedGameData] = useState("");
   const [selectedTeam, setSelectedTeam] = useState(121);
+  const [selectedGroup, setSelectedGroup] = useState("pitching");
   const [teamLogo, setTeamLogo] = useState(121);
   const TeamImageSrc = `./Images/logos/${teamLogo}.svg`;
 
   useEffect(() => {
     fetch(
-      `https://statsapi.mlb.com/api/v1/stats?stats=lastXGames&group=pitching&teamId=${selectedTeam}`
+      `https://statsapi.mlb.com/api/v1/stats?stats=lastXGames&group=${selectedGroup}&teamId=${selectedTeam}`
     )
       .then((resp) => resp.json())
       .then((data) => {
         setFetchedGameData(data.stats[0].splits);
       });
-  }, [selectedTeam]);
+  }, [selectedTeam, selectedGroup]);
 
   if (!fetchedGameData.length) {
     return <h1>Loading...</h1>;
@@ -40,19 +41,62 @@ function AdvancedPitching() {
             alt={`Headshot of ${user.player.fullName}`}
             src={`https://img.mlbstatic.com/mlb-photos/image/upload/v1/people/${user.player.id}/headshot/silo/current`}
           ></img>
+
           <span className="ABSpanName">{user.player.fullName}</span>
-          <span className="ABSpan">{`${user.stat.wins} : Wins`}</span>
-          <span className="ABSpan">{`${user.stat.wildPitches} : Wild Pitches`}</span>
-          <span className="ABSpan">{`${user.stat.walksPer9Inn} : Walks Per 9 Innings `}</span>
-          <span className="ABSpan">{`${user.stat.strikePercentage} : Percentage of Pitches that are strikes`}</span>
-          <span className="ABSpan">{`${user.stat.pitchesPerInning} : Pitches Per Inning`}</span>
-          <span className="ABSpan">{`${user.stat.era} :  ERA over the past 10 apperances`}</span>
-          <span className="ABSpan">{`${user.stat.battersFaced} : Batters Faced`}</span>
-          <span className="ABSpan">{`${user.stat.groundOutsToAirouts} : Ground outs to Air outs`}</span>
-          <span className="ABSpan">{`${user.stat.hitByPich} : batters hit by pitches`}</span>
-          <span className="ABSpan">{`${user.stat.hitsPer9Inn} : Hits per 9`}</span>
-          <span className="ABSpan">{`${user.stat.whip} Walks and Hits per inning pitched`}</span>
-          <span className="ABSpan">{`${user.stat.slg} Slugging against`}</span>
+          <div className="bpStatWrapper">
+            <span className="bpStatFeild">{`Wins`}</span>
+            <span className="bpStatDataSpan">{user.stat.wins}</span>
+          </div>
+          <div className="bpStatWrapper">
+            <span className="bpStatFeild">{`Wild Pitches`}</span>
+            <span className="bpStatDataSpan">{user.stat.wildPitches}</span>
+          </div>
+          <div className="bpStatWrapper">
+            <span className="bpStatFeild">{`Walks Per 9 Innings`}</span>
+            <span className="bpStatDataSpan">{user.stat.walksPer9Inn}</span>
+          </div>
+          <div className="bpStatWrapper">
+            <span className="bpStatFeild">{`% of Pitches that are strikes`}</span>
+            <span className="bpStatDataSpan">{user.stat.strikePercentage}</span>
+          </div>
+          <div className="bpStatWrapper">
+            <span className="bpStatFeild">{`Pitches Per Inning`}</span>
+            <span className="bpStatDataSpan">{user.stat.pitchesPerInning}</span>
+          </div>
+          <div className="bpStatWrapper">
+            <span className="bpStatFeild">{`ERA over the past 10 apperances`}</span>
+            <span className="bpStatDataSpan">{user.stat.era}</span>
+          </div>
+          <div className="bpStatWrapper">
+            <span className="bpStatFeild">{`Batters Faced`}</span>
+            <span className="bpStatDataSpan">{user.stat.battersFaced}</span>
+          </div>
+          <div className="bpStatWrapper">
+            <span className="bpStatFeild">{`Ground outs to Air outs`}</span>
+            <span className="bpStatDataSpan">
+              {user.stat.groundOutsToAirouts}
+            </span>
+          </div>
+          <div className="bpStatWrapper">
+            <span className="bpStatFeild">{`batters hit by pitches`}</span>
+            <span className="bpStatDataSpan">{user.stat.hitByPich}</span>
+          </div>
+          <div className="bpStatWrapper">
+            <span className="bpStatFeild">{`Hits per 9`}</span>
+            <span className="bpStatDataSpan">{user.stat.hitsPer9Inn}</span>
+          </div>
+          <div className="bpStatWrapper">
+            <span className="bpStatFeild">{`Walks and Hits per inning pitched`}</span>
+            <span className="bpStatDataSpan">{user.stat.whip}</span>
+          </div>
+          <div className="bpStatWrapper">
+            <span className="bpStatFeild">{`Slugging against`}</span>
+            <span className="bpStatDataSpan">{user.stat.slg}</span>
+          </div>
+          <div className="bpStatWrapper">
+            <span className="bpStatFeild">{`Batters Faced`}</span>
+            <span className="bpStatDataSpan">{user.stat.battersFaced}</span>
+          </div>
         </div>
       ));
     }
