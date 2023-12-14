4a9d09dbadcfa36f5bf6e2ba91b96b91a3a8dbb3
Sam Peck <fullstacksam@protonmail.com>
2023-12-14 01:44:13 -0500

    User name cut short if long

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -12,7 +12,10 @@ return (
         <NavLink to='/player/'>Players</NavLink>
         <img className="logo" alt="life sized baseball in old french generals hat" src={logoLoc}></img>
         <NavLink to='/leaderboard'>LeaderBoard</NavLink>
-        { user ? <NavLink to={`/user/${user.username}`} user={user}>{user.username}</NavLink> : null }
+        { user ? 
+            <NavLink to={`/user/${user.username}`} user={user}>
+                {user.username.length > 15 ? user.username.substring(0,9) + '...' : user.username}
+            </NavLink> : null }
         { user ? <SignOut user={user} setUser={setUser} /> : <NavLink to='/login'>Login</NavLink> }
     </div>
 )
