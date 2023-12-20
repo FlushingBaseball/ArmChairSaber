23592ade24c12749a40c31992edc8ca56edbd565
Sam Peck <fullstacksam@protonmail.com>
2023-12-20 03:45:35 -0500

    API url change and predictions work

--- server/predictions.py
+++ server/predictions.py
@@ -3,20 +3,23 @@ import asyncio
 
 
 
-# def fetch_unresolved_predictions_pool():
-#   response = requests.get('/api/predictionsNotResolved')
-#   print(response)
 
 
+
+
+
+
+## requesting all unresolved Predictions
 async def handle_unresolved_predictions_pool():
   try:
      response = requests.get('http://localhost:5555/api/predictionsNotResolved')
 
-
      if response.status_code ==200:
       print("response from the server")
-      data = response.json()
-      print(data)
+      data = response.json() 
+      print("data recieved")
+      # print(f"data is now of type {type(data)}") ## List
+      check_for_winners(data)
 
      else:
       print(f"Error: {response.status_code} - {response.text}")
@@ -25,21 +28,51 @@ async def handle_unresolved_predictions_pool():
     print(f"Request failed: {e}")
 
 
+
+
+
+
+def check_for_winners(data):
+  print(f"Successfully called CFW")
+  for prediction in data:
+    print(prediction)
+    if (prediction['actualWinnerId'] != None):
+      handleWinnerKnown(prediction)
+    else:
+      print(f"Prediction actualWinnerId property is {prediction['actualWinnerId']}")
+      handle_winner_not_known(prediction)
+
+
+    
+
+def handle_winner_not_known(prediction):
+  game_id = prediction['game_Id']
+  game_response = requests.get('http://localhost:5555/api/games/{game_id}')
+
+  if  game_response.status_code == 200:
+    print("200 from server")
+    game_data = game_response.json()
+    print(f'Server Game Data: {game_data}')
+
+  else:
+    print("Game Wasn't on backend")
+    print(f"Error: {game_response.status_code} - {game_response.text}")
+  
+
+
+## for every prediction 
+## check if theres actualwinner on the prediction? 
+## if theres not await the response from the backend
+## if the backend has the game assign actualwinner and actualloser to the predicion and grade it 
+## send the patched prediction with that data to the backend
+
+## if the backend doesnt have the game 
+## fetch that game from the external api 
+## assign actualWinner and actualLoser to the predicion from 
+## game data we fetched, Grade the prediction and send it patched
+## to the backend 
+
+
 asyncio.run(handle_unresolved_predictions_pool())
 
-# backend_url = 'http://localhost:5555/'
-# endpoint = '/api/predictionsNotResolved'
-# url = f'{backend_url}{endpoint}'
 
-# try:
-#     response = requests.get(url)
-
-#     # Check if the request was successful (status code 200)
-#     if response.status_code == 200:
-#         print("Response from the server:")
-#         print(response.json())  # Assuming the response is in JSON format
-#     else:
-#         print(f"Error: {response.status_code} - {response.text}")
-
-# except requests.RequestException as e:
-#     print(f"Request failed: {e}")
