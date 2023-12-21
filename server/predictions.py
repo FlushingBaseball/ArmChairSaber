59c8843556b26a53efc90a40e6c1e55f475393e8
Sam Peck <fullstacksam@protonmail.com>
2023-12-20 23:24:32 -0500

    call returned from mlb

--- server/predictions.py
+++ server/predictions.py
@@ -46,29 +46,52 @@ def check_for_winners(data):
     
 
 def handle_winner_not_known(prediction):
+  print("Successfully called Handle Winner Not Known")
   game_id = prediction['game_Id']
   game_response = requests.get('http://localhost:5555/api/games/{game_id}')
 
   if  game_response.status_code == 200:
     print("200 from server")
     game_data = game_response.json()
-    print(f'Server Game Data: {game_data}')
+    # print(f'Server Game Data: {game_data}')
+
 
   else:
     print("Game Wasn't on backend")
-    print(f"Error: {game_response.status_code} - {game_response.text}")
+    # print(f"Error: {game_response.status_code} - {game_response.text}")
+    # call_mlb_patch_prediction(prediction, game_id)
+    call_mlb_patch_prediction(prediction, game_id)
+
+
+
+def call_mlb_patch_prediction(prediction, game_id):
+  print("Successfully called: call_mlb_patch_prediction")
+  # print(f'prediction parameter {prediction} - game_id is {game_id}')
+  # print(f'Type of game_id paramater {type(str(game_id))}')
+  mlb_Data = requests.get(f'https://statsapi.mlb.com/api/v1/schedule?sportId=1&gamePk={str(game_id)}')
+  # print(f'MLB response {mlb_Data}')
+
+  if mlb_Data.status_code ==200:
+    print("200 from MLB")
+    mlb_game_response = mlb_Data.json()
+    print(f'This is the response from MLB: {mlb_game_response}')
+    print(f'The type of the response is: {type(mlb_game_response)}')
   
+  else:
+    print(f"Error: {mlb_Data.status_code} - {mlb_Data.text}")
+
+
 
 
 ## for every prediction 
 ## check if theres actualwinner on the prediction? 
 ## if theres not await the response from the backend
-## if the backend has the game assign actualwinner and actualloser to the predicion and grade it 
+## if the backend has the game assign actualwinner and actualloser to the prediction and grade it 
 ## send the patched prediction with that data to the backend
 
 ## if the backend doesnt have the game 
 ## fetch that game from the external api 
-## assign actualWinner and actualLoser to the predicion from 
+## assign actualWinner and actualLoser to the prediction from 
 ## game data we fetched, Grade the prediction and send it patched
 ## to the backend 
 
