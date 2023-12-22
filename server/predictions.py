8a6923d7a0def5f167ee3fc67658fd4c69a8efcb
Sam Peck <fullstacksam@protonmail.com>
2023-12-21 23:46:57 -0500

    blah

--- server/predictions.py
+++ server/predictions.py
@@ -6,10 +6,7 @@ import asyncio
 
 users_streak_cache = {}
 
-
-
-
-## requesting all unresolved Predictions
+## Fetching all unresolved predictions
 async def handle_unresolved_predictions_pool():
   try:
      response = requests.get('http://localhost:5555/api/predictionsNotResolved')
@@ -19,7 +16,6 @@ async def handle_unresolved_predictions_pool():
       data = response.json() 
       print("data recieved")
       check_for_winners(data)
-
      else:
       print(f"Error: {response.status_code} - {response.text}")
 
@@ -31,55 +27,92 @@ async def handle_unresolved_predictions_pool():
 def check_for_winners(data):
   print(f"Successfully called CFW")
   for prediction in data:
+    ## building out streak cache 
     if prediction['user']['id'] in users_streak_cache:
       print("had this user")
     else:
       print("didn't have this user")
       users_streak_cache[prediction['user']['id']] = prediction['user']['currentStreak']
-
+    ## Looping through predictions and checking for ActualWinnerData contamination 
     if (prediction['actualWinnerId'] != None):
       handleWinnerKnown(prediction)
     else:
-      # print(f"Prediction actualWinnerId property is {prediction['actualWinnerId']}")
       handle_winner_not_known(prediction)
 
 
     
 
 def handle_winner_not_known(prediction):
-  print("Successfully called Handle Winner Not Known")
+
   game_id = prediction['game_Id']
   game_response = requests.get('http://localhost:5555/api/games/{game_id}')
 
   if  game_response.status_code == 200:
     print("200 from server")
     game_data = game_response.json()
-    # print(f'Server Game Data: {game_data}')
-
-
+    print(f'Server Game Data: {game_data}')
   else:
-    print("Game Wasn't on backend")
-    # print(f"Error: {game_response.status_code} - {game_response.text}")
-    # call_mlb_patch_prediction(prediction, game_id)
-    # call_mlb_patch_prediction(prediction, game_id)
-    print(users_streak_cache)
+    print("Game wasn't in database")
+    call_mlb_patch_prediction(prediction, game_id)
+    # print(users_streak_cache)
 
 
 
 def call_mlb_patch_prediction(prediction, game_id):
-  print("Successfully called: call_mlb_patch_prediction")
-  # print(f'prediction parameter {prediction} - game_id is {game_id}')
-  # print(f'Type of game_id paramater {type(str(game_id))}')
+  print("In: call_mlb_patch_prediction")
+
   mlb_Data = requests.get(f'https://statsapi.mlb.com/api/v1/schedule?sportId=1&gamePk={str(game_id)}')
-  # print(f'MLB response {mlb_Data}')
 
   if mlb_Data.status_code ==200:
-    print("200 from MLB")
     mlb_game_response = mlb_Data.json()
     print(f'This is the response from MLB: {mlb_game_response}')
-    print(f'The type of the response is: {type(mlb_game_response)}')
-    # if 
+    if (not mlb_game_response['dates'][0]['games'][0]['teams']['away']["isWinner"] and mlb_game_response["dates"][1] and not mlb_game_response['dates'][1]['games']['teams']['away']['isWinner']):
+      print("game has no winner")
+      return
+    else:
+      print("game has a winner!")
+    
+    
+    
+
+# if prediction['user']['id'] in users_streak_cache:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    if mlb_game_response[dates][1] in mlb_game_response:
+      print("Multi date game")
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   else:
     print(f"Error: {mlb_Data.status_code} - {mlb_Data.text}")
 
