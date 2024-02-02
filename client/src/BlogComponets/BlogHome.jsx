7a763e91960ef72cc5ff1870f4762251e152c6c6
Sam Peck <fullstacksam@protonmail.com>
2024-02-02 00:25:19 -0500

    Blog layout test, logo and nothing

--- client/src/BlogComponets/BlogHome.jsx
+++ client/src/BlogComponets/BlogHome.jsx
@@ -1,18 +1,32 @@
-import { useState } from "react"
-
-import RichHill from './Articles/RichHill.mdx'
-
-
-export default function BlogHome(){
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 
+import RichHill from "./Articles/RichHill.mdx";
 
+export default function BlogHome() {
   return (
     <div className="WrapperBlogHome">
-   
-        <RichHill />
-     
+      
+
+      <div className="Wrapper-Recent-Posts">
+        <h3 id="Recent-Articles-Header">Recent Articles</h3>
+        <Link className="Preview-Article-Title" to={`/blog/Rich-Hill`}>
+          {" "}
+          Rich Hill
+        </Link>
+        <Link className="Preview-Article-Title" to={`/blog/MLB-Expansion`}>
+          {" "}
+          MLB Expansion
+        </Link>
+      </div>
+
+
+
+ 
+      <RichHill />
+
       {/* <ArticleOne /> */}
     </div>
-  )
+  );
 }
