5df6f74a69968d18a9b2c6c7abed5443ce06eadd
Sam Peck <fullstacksam@protonmail.com>
2024-02-08 23:33:43 -0500

    Recent articles made into standaloneand path issue

--- client/src/BlogComponets/BlogHome.jsx
+++ client/src/BlogComponets/BlogHome.jsx
@@ -1,32 +1,15 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+// import { useState } from "react";
 
+import RecentArticles from "./RecentArticles";
 
 import RichHill from "./Articles/RichHill.mdx";
-import MLBExpansion from "./Articles/MLBExpansion.mdx"
+// import MLBExpansion from "./Articles/MLBExpansion.mdx"
 
 export default function BlogHome() {
   return (
     <div className="WrapperBlogHome">
-      
-
-      <div className="Wrapper-Recent-Posts">
-        <h3 id="Recent-Articles-Header">Recent Articles</h3>
-        <Link className="Preview-Article-Title" to={`/blog/Rich-Hill`}>
-          {" "}
-          Rich Hill
-        </Link>
-        <Link className="Preview-Article-Title" to={`/blog/MLB-Expansion`}>
-          {" "}
-          MLB Expansion
-        </Link>
-      </div>
-      
-
-
- 
-      <RichHill />
-
+      <RecentArticles />
+        <RichHill />
     </div>
   );
 }
---
+++ client/src/BlogComponets/RecentArticles.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+export default function RecentArticles() {
+  return (
+    <div className="Wrapper-Recent-Posts">
+      <h3 id="Recent-Articles-Header">Recent Articles</h3>
+      <Link className="Preview-Article-Title" to={`/blog/Rich-Hill`}>
+        {" "}
+        Rich Hill
+      </Link>
+      <Link className="Preview-Article-Title" to={`/blog/MLB-Expansion`}>
+        {" "}
+        MLB Expansion
+      </Link>
+    </div>
+  );
+}
