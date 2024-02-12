d1474e005659b898ed4f07fc249b40471d441dee
Sam Peck <fullstacksam@protonmail.com>
2024-02-12 14:39:27 -0500

    card css temp fix

--- client/src/BlogComponets/Article-Data.js
+++ client/src/BlogComponets/Article-Data.js
@@ -1,18 +1,18 @@
 // data.js
 const firstCardData = {
   // Your data properties here
-  frontText: 'In 2021 Aaron Loup had a .95 ERA over 56.2 innings, Flip this card to see what he was in 2022',
-  backText: 'He was much worse! he had a 3.84 ERA over 58.2 innings',
+  frontText: 'In 2021 Aaron Loup had a .95 ERA over 56.2 innings,',
+  backText: 'In 2022 he had a 3.84 ERA over 58.2 innings',
 };
 const secondCardData = {
   // Your data properties here
-  frontText: '2016 season with the Yankees, Aroldis Chapman faced struggles, with a 2.01 WHIP and a 2.01 ERA. He was subsequently traded mid-season to the Chicago Cubs.',
-  backText: `After joining the Chicago Cubs, Chapman's performance significantly improved. He played a pivotal role in the Cubs' World Series run, recording crucial saves and finishing the season with a 1.55 ERA.`,
+  frontText: 'Jesse Chavez 2019-23 for teams other than the Braves ERA 6.32 ',
+  backText: `On the Braves 2.17, over three sperate stints with other teams in between`,
 };
 const thirdCardData = {
   // Your data properties here
-  frontText: `In the first half of the 2014 with the Red Sox, Andrew Miller posting a 2.34 WHIP and a 2.34 ERA.`,
-  backText: `Following the trade to the Orioles, he recorded a 1.35 ERA and a 0.95 WHIP. He played a crucial role in the Orioles' playoff run.`,
+  frontText: `Jake Diekman, 31 IP 7.04 ERA for CHW in 22-23`,
+  backText: `Traded to the Rays 45 IP 2.18 ERA`,
 };
 
 export {firstCardData, secondCardData, thirdCardData}
--- client/src/BlogComponets/ArticleInteractions/FlipCard.jsx
+++ client/src/BlogComponets/ArticleInteractions/FlipCard.jsx
@@ -1,32 +1,45 @@
-import { useState } from "react"
-import {useSpring, a} from '@react-spring/web'
+import { useState } from "react";
+import { useSpring, a } from "@react-spring/web";
+import { handleImageError } from "../../UtilityFunctions/UtilityFunctions";
 
-export default function FlipCard({frontText, backText}){
+export default function FlipCard({ frontText, backText, playerId }) {
+  const [flipped, setFlipped] = useState(false);
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotatex(${flipped ? 180 : 0}deg)`,
+    config: { mass: 10, tension: 900, friction: 80 },
+  });
 
-const [flipped, setFlipped] = useState(false)
-const {transform, opacity} = useSpring({
-  opacity: flipped ? 1 : 0,
-  transform: `perspective(600px) rotatex(${flipped ? 180 : 0}deg)`,
-  config: {mass: 10, tension: 900, friction: 80},
-
-})
+  const playerImage = `https://img.mlbstatic.com/mlb-photos/image/upload/v1/people/${playerId}/headshot/silo/current`;
 
   return (
-    <div className="Wrapper-Flip-Card" onClick={() => setFlipped(state => !state)}>
+    <div
+      className="Wrapper-Flip-Card"
+      onClick={() => setFlipped((state) => !state)}
+    >
       <a.div
         className="Flip-Card  Flip-Card-Front"
-        style={{ opacity: opacity.to(o => 1 - o), transform }}
-      ><span className="FlipSpan">{frontText}</span></a.div>
+        style={{ opacity: opacity.to((o) => 1 - o), transform }}
+      >
+        {playerId ? (
+          <img
+            id="Flip-Card-Image"
+            src={playerImage}
+            onError={(e) => handleImageError(e.target, playerId)}
+          ></img>
+        ) : null}
+        <span className="FlipSpan">{frontText}</span>
+      </a.div>
       <a.div
         className="Flip-Card  Flip-Card-Back"
         style={{
           opacity,
           transform,
-          rotateX: '180deg',
+          rotateX: "180deg",
         }}
-      ><span className="FlipSpan">{backText}</span></a.div>
+      >
+        <span className="FlipSpan">{backText}</span>
+      </a.div>
     </div>
-  )
+  );
 }
-
-  
