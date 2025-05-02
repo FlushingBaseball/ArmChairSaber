b145f70b87310f5946898918564ef44e44da3c76
Sam Peck <fullstacksam@protonmail.com>
2025-05-02 05:50:43 -0400

    Remade standings

--- client/src/TeamComponents/Standings.jsx
+++ client/src/TeamComponents/Standings.jsx
@@ -1,17 +1,16 @@
 import { useState, useEffect } from "react";
 
+    /**
+     * TODO give users the option what standings to display be default
+     * TODO expand options to display all team affiliates
+     */
+
 export default function Standings({ selectedTeam }) {
   const [standingData, setStandingData] = useState();
 
-//byLeague
-//byOrganization
-//byDivision
-
-
-
   useEffect(() => {
     fetch(
-      `https://statsapi.mlb.com/api/v1/standings?leagueId=103,104&season=2025&standingsTypes=regularSeason,wildCard`
+      `https://statsapi.mlb.com/api/v1/standings?leagueId=103,104&season=2025&standingsTypes=regularSeason,wildCard&hydrate=division`
     )
       .then((resp) => resp.json())
       .then((data) => {
@@ -19,10 +18,9 @@ export default function Standings({ selectedTeam }) {
       });
   }, []);
 
-  // useEffect(() => {
-  //   console.log("in use Effect standingData")
-  //   console.log(standingData);
-  // }, [standingData]);
+  useEffect(() => {
+    console.log(standingData);
+  }, [standingData]);
 
   if (standingData === ''){
     return(
@@ -33,35 +31,62 @@ export default function Standings({ selectedTeam }) {
 
   function mapStandings(updatedData) {
     if (updatedData === undefined) return null;
+      const teamRelevantStandings = [];
+      const otherStandings = [];
+
+    for (const record of updatedData.records) {
+      const containsSelectedTeam = record.teamRecords.some(teamRecord => 
+        teamRecord.team.id === selectedTeam
+      )
+      if (containsSelectedTeam){
+        teamRelevantStandings.push(record)
+      }
+      else {
+        otherStandings.push(record)
+      }
+    }
+
+    const sortedRecords = [...teamRelevantStandings, ...otherStandings];
+
+
+
   
-    const tables = updatedData.records.map((recordGrouping) => {
+    const tables = sortedRecords.map((recordGrouping) => {
       const rows = recordGrouping.teamRecords.map((team) => (
-        <tr className={selectedTeam === team.team.id ? "highlightTeam" : null} key={team.team.id}>
-          <th id="logoTh"><img id="standingLogo" src={`https://www.mlbstatic.com/team-logos/${team.team.id}.svg`} alt={team.team.name} /></th>
-          <th id="teamNameTh">{team.team.name}</th>
-          <th>{team.wins}</th>
-          <th>{team.losses}</th>
-          <th>{team.winningPercentage}</th>
-          <th>{team.streak.streakCode}</th>
-          <th>{team.runDifferential}</th>
+        <tr className={selectedTeam === team.team.id ? "Standings-Highlight-Team" : null} key={team.team.id}>
+          <td className="Standings-Table-Data" id="Standings-Logo-Table-Header">
+            <img id="Standings-Logo"
+                 src={`/api/team_logo_images/${team.team.id}_old_score.webp`}
+                 alt={team.team.name} /></td>
+          <td className="Standings-Table-Data" id="teamNameTh">{team.team.name}</td>
+          <td className="Standings-Table-Data">{team.wins}</td>
+          <td className="Standings-Table-Data">{team.losses}</td>
+          <td className="Standings-Table-Data">{team.winningPercentage}</td>
+          <td className="Standings-Table-Data">{team.streak.streakCode}</td>
+          <td className="Standings-Table-Data">{team.runDifferential}</td>
         </tr>
       ));
   
       return (
-        <table id="standingsTable" key={Math.random()}>
-          <thead id="standingsHead">
-            <tr>
-              <th className="teamLogoTh"></th>
-              <th className="team-th">Team</th>
-              <th>Wins</th>
-              <th>Loses</th>
-              <th>%</th>
-              <th>Streak</th>
-              <th>Run Differential</th>
-            </tr>
-          </thead>
-          <tbody id="standingsBody">{rows}</tbody>
-        </table>
+        <div className="Single-Standing-Wrapper">
+          <div className="League-Name-Span">
+          {recordGrouping.division.nameShort ? recordGrouping.division.nameShort : "Wild Card"}
+            </div>
+          <table className="Standings-Table" key={`${recordGrouping.division.id} + ${recordGrouping.teamRecords[0].team.id}`}>
+            <thead id="standingsHead">
+              <tr>
+                <th className="Team-Logo-Table-Header"></th>
+                <th className="Standings-Table-Header">Team</th>
+                <th className="Standings-Table-Header">Wins</th>
+                <th className="Standings-Table-Header">Loses</th>
+                <th className="Standings-Table-Header">%</th>
+                <th className="Standings-Table-Header">Streak</th>
+                <th className="Standings-Table-Header">Run Differential</th>
+              </tr>
+            </thead>
+            <tbody id="standingsBody">{rows}</tbody>
+          </table>
+        </div>
       );
     });
   
@@ -71,10 +96,20 @@ export default function Standings({ selectedTeam }) {
 
   return (
     <div className="WrapperStandings">
-      <h1>League Standings</h1>
+      <div className="Standings-Header-Wrapper">
+        <img
+          className="Standings-Old-Score-Logo"
+          src="/api/team_logo_images/logo_old_score.webp"
+          alt="Orange LCD dot matrix baseball in a Napoleonic Hat"
+        />
+          <span className="League-Standings-Header">League Standings</span>
+        <img
+          className="Standings-Old-Score-Logo"
+          src="/api/team_logo_images/logo_old_score.webp"
+          alt="Orange LCD dot matrix baseball in a Napoleonic Hat"
+        />
+      </div>
       {mapStandings(standingData)}
-
-
     </div>
   );
 }
